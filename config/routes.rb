4826fe56270239d723ea53e64ae00c3118ce7825
Kevin <kevinsevinche@gmail.com>
2021-11-30 12:38:15 -0500

    Setup routes

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'greetings/index'
+    end
+  end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1, defaults: { format: 'json' } do
+      get 'greetings', to: 'greetings#index'
+    end
+  end
+end
